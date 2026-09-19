ac1b9bbada4b0d736ec2fb475fde4a801ed3edf4
Allison <allisonsouza10261@gmail.com>
2026-09-19 16:20:19 -0300

    start project

---
+++ src/main/java/com/br/meuimovel/MeuImovelApplication.java
@@ -0,0 +1,13 @@
+package com.br.meuimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuImovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuImovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/meuimovel/MeuImovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.meuimovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuImovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
