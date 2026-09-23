5365b6822f908adaa23256d411db6a94afadf025
Allison <allisonsouza10261@gmail.com>
2026-09-23 20:09:26 -0300

    feat : personalizar exceptions para tratar erros de login

--- src/main/java/com/br/meuimovel/exception/GlobalExceptionHandler.java
+++ src/main/java/com/br/meuimovel/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.br.meuimovel.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,23 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+        ErrorResponse errorResponse = new ErrorResponse
+                ("Email ou senha inválidos", HttpStatus.UNAUTHORIZED,
+                        LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ErrorResponse> handleDisabledException(DisabledException exception) {
+        ErrorResponse errorResponse = new ErrorResponse
+                ("Usuario inativo ou bloqueado", HttpStatus.UNAUTHORIZED,
+                        LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
     @ExceptionHandler(CidadeNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCidadeNotFoundException(CidadeNotFoundException exception){
         ErrorResponse errorResponse = new ErrorResponse
@@ -17,14 +36,6 @@ public class GlobalExceptionHandler {
                         LocalDateTime.now());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler(CidadeAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> handleCidadeAlreadyExistsException(CidadeAlreadyExistsException exception){
-        ErrorResponse errorResponse = new ErrorResponse
-                (exception.getMessage(), HttpStatus.CONFLICT,
-                        LocalDateTime.now());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(EmpresaNotFoundException.class)
@@ -35,6 +46,31 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+    @ExceptionHandler(UsuarioNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUsuarioNotFoundException(UsuarioNotFoundException exception){
+        ErrorResponse errorResponse = new ErrorResponse
+                (exception.getMessage(), HttpStatus.NOT_FOUND,
+                        LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CidadeAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleCidadeAlreadyExistsException(CidadeAlreadyExistsException exception){
+        ErrorResponse errorResponse = new ErrorResponse
+                (exception.getMessage(), HttpStatus.CONFLICT,
+                        LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(UsuarioAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUsuarioAlreadyExistsException(UsuarioAlreadyExistsException exception){
+        ErrorResponse errorResponse = new ErrorResponse
+                (exception.getMessage(), HttpStatus.CONFLICT,
+                        LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
     @ExceptionHandler(EmpresaAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmpresaAlreadyExistsException(EmpresaAlreadyExistsException exception){
         ErrorResponse errorResponse = new ErrorResponse
@@ -43,6 +79,17 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(UsuarioNaoPodePossuirEmpresaException.class)
+    public ResponseEntity<ErrorResponse> handleUsuarioNaoPodePossuirEmpresaException
+            (UsuarioNaoPodePossuirEmpresaException exception){
+        ErrorResponse errorResponse = new ErrorResponse
+                (exception.getMessage(), HttpStatus.CONFLICT,
+                        LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
     @ExceptionHandler(DocumentoInvalidoException.class)
     public ResponseEntity<ErrorResponse> handleDocumentoInvalidoException(DocumentoInvalidoException exception){
         ErrorResponse errorResponse = new ErrorResponse
